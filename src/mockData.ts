9b0187b4a48d4abbc03422b04b43f0630679849e
IngvarrFX <nurov.mj@gmail.com>
2022-06-20 17:52:18 +0300

    create mock data for app

---
+++ src/mockData.ts
@@ -0,0 +1,57 @@
+import cat from "./assets/cat.png";
+
+export const data: Array<DataType> = [
+    {
+        id: "1",
+        tagline: "Сказочное заморское яство",
+        title: "Нямушка",
+        withWhatFeed: "с фуа-гра",
+        measureQuantity: 10,
+        measure: "порций",
+        promotion: "мышь в подарок",
+        weight: "0,5",
+        unitWeight: "кг",
+        description: "Печень утки разварная с артишоками.",
+        image: cat,
+    },
+    {
+        id: "2",
+        tagline: "Сказочное заморское яство",
+        title: "Нямушка",
+        withWhatFeed: "с рыбой",
+        measureQuantity: 40,
+        measure: "порций",
+        promotion: "2 мыши в подарок",
+        weight: "2",
+        unitWeight: "кг",
+        description: "Головы щучьи с чесноком да свежайшая сёмгушка.",
+        image: cat,
+    },
+    {
+        id: "3",
+        tagline: "Сказочное заморское яство",
+        title: "Нямушка",
+        withWhatFeed: "с курой",
+        measureQuantity: 100,
+        measure: "порций",
+        promotion: "5 мышей в подарок заказчик доволен",
+        weight: "5",
+        unitWeight: "кг",
+        description: "Печалька, с курой закончился.",
+        image: cat,
+    },
+]
+
+export type DataType = {
+    id: string
+    tagline: string
+    title: string
+    withWhatFeed: string
+    measureQuantity:number
+    measure: string
+    promotion: string
+    weight: string
+    unitWeight: string
+    description: string
+    image: string
+}

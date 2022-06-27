f116812f89cf7ec050c672a621d116655c9a928a
IngvarrFX <nurov.mj@gmail.com>
2022-06-27 10:45:32 +0300

    add extra class and styles

--- src/components/Weight/Weight.tsx
+++ src/components/Weight/Weight.tsx
@@ -4,14 +4,17 @@ import styles from "./Weight.module.scss";
 type WeightPropsType = {
     weight: string
     unitWeight: string
+    isStock: boolean
 }
 
 export const Weight = (props: WeightPropsType) => {
 
-    const {weight, unitWeight} = props;
+    const {weight, unitWeight, isStock} = props;
+
+    const style = isStock ? styles.wrapper : styles.wrapper + " " + styles.dark;
 
     return (
-        <div className={styles.wrapper}>
+        <div className={style}>
             <div className={styles.weight}>{weight}</div>
             <div className={styles.unit}>{unitWeight}</div>
         </div>
